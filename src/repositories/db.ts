e40e5de453c2b3b3b43386803fe4e22103f4b8fc
safronman <safronmanbox@gmail.com>
2022-10-31 14:25:58 +0300

    ✅ start connect to MongoDb

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,6 +1,4 @@
 import { MongoClient, ObjectId } from 'mongodb'
-import * as dotenv from 'dotenv'
-dotenv.config()
 
 export type ProductType = {
   title: string
@@ -8,25 +6,18 @@ export type ProductType = {
 }
 
 // Connection URL
-const url = process.env.MONGO_URL;
+const url = 'mongodb://localhost:27017';
 console.log('url :', url)
-if (!url) {
-  throw Error('dDon\'t corect URL')
-}
 const client = new MongoClient(url);
 
-// Database Name
-const db = client.db()
-
-
-export const productCollection = db.collection<ProductType>('products');
+export const productCollection = client.db().collection<ProductType>('products');
 
 export const runDb = async () => {
   try {
     await client.connect();
-    console.log('Connected successfully to server');
+    console.log('✅ Connected successfully to server');
   } catch (e) {
-    console.log('Don\'t connected successfully to server');
+    console.log('❗ Don\'t connected successfully to server');
     await client.close()
   }
 };
